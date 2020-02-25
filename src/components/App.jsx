4fcfeb4de3ca96227dbf7c27a1095552df286e65
Nick Mc <nbmc@protonmail.com>
2020-02-25 11:32:31 -0600

    installed isomorphic-fetch and es6-promise. added componentDidMount() method to App.jsx.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
+import 'isomorphic-fetch';
+import 'es6-promise';
+
+const BASE_URL = 'http://ghibliapi.herokuapp.com/';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
+    // constructor(props) {
+    //     super(props);
+    // }
+    componentDidMount() {
+        fetch(BASE_URL + 'films')
+        .then(response => response.json())
+        .then(object => console.log(object));
     }
     render() {
         return (
